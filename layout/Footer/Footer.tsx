aaca14dec95932c6017eed70190c89b88fc67631
ostaoleho <oleh.ostashevskyi@otakoyi.com.ua>
2021-11-08 10:46:06 +0200

    finished CSS Grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,18 @@
 import { FooterProps } from "./Footer.props";
-import styles from "./Layout.module.css";
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
 export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+  console.log(props);
+  return (
+    <footer {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Всі права захищені</div>
+      <a href="3" target="_blank">
+        Угода користувача
+      </a>
+      <a href="#" target="_blank">
+        Політика конфіденційності
+      </a>
+    </footer>
+  );
 };
